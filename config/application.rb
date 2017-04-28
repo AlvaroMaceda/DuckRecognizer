51644f00b0a6fbb3751caa7a7313ed9619483a35
Alvaro Maceda <alvaro@alvaromaceda.es>
2017-04-28 22:07:04 +0200

    Changed ruby version to 2.4.0. Disabled assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +13,8 @@ module DuckRecognizer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Disable Asset Pipeline/Sprockets
+    config.assets.enabled = false
   end
 end
